b21f1b247a01dcd2f4fb8de0c9c55c2926f40bf5
johnnaksaing <johnnaksaing@daum.net>
2022-08-02 17:48:53 +0900

    todo : combo
    이미지는 컨셉용으로 그냥 가져온거라 인게임에 쓰면 안되고 완료전에 지워야 함

--- Assets/Scripts/ShefController.cs
+++ Assets/Scripts/ShefController.cs
@@ -2,31 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Combo Manager
 public class ShefController : MonoBehaviour
 {
-    GameObject[] pfb_Sushies;
+    //maybe this would help..?
+    //https://www.youtube.com/watch?v=avl2bSyL9ZA
     public enum E_InputCommand
     {
         a = 1,
-        d = 2
+        s = 2,
+        d = 3
     };
+    List<GameObject> pfb_Sushies;
     Queue<E_InputCommand> q;
+    Dictionary<E_InputCommand[], E_InputCommand[]> dict;
+
     // Start is called before the first frame update
     void Start()
     {
+        pfb_Sushies = new List<GameObject>();
         q = new Queue<E_InputCommand>();
+        dict = new Dictionary<E_InputCommand[], E_InputCommand[]>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.anyKeyDown) 
         {
-            q.Enqueue(E_InputCommand.a);
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                q.Enqueue(E_InputCommand.a);
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                q.Enqueue(E_InputCommand.s);
+            }
+            else if (Input.GetKeyDown(KeyCode.D)) 
+            {
+                q.Enqueue(E_InputCommand.d);
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.D)) 
-        {
-            q.Enqueue(E_InputCommand.d);
-        }
+
     }
+
 }
